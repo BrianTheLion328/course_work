1a7e272dfe35d696558d4a550361b861198663f7
BrianTheLion328 <78449517+BrianTheLion328@users.noreply.github.com>
2021-04-04 13:51:57 -0700

    Add files via upload

--- project_08/user_hub/app.js
+++ project_08/user_hub/app.js
@@ -135,3 +135,71 @@ function bootstrap() {
 
 
 bootstrap();
+
+function setCommentsOnPost(post) {
+  // if we already have comments, don't fetch them again
+  if (post.comments) {
+    return Promise.reject(null);
+  }
+  // fetch, upgrade the post object, then return it
+  return fetchPostComments(post.id).then(function (comments) {
+    post.comments = comments;
+    return post;
+  });
+}
+function renderPost(post) {
+  return $(`<div class="post-card">
+    <header>
+      <h3>${ post.title }</h3>
+      <h3>--- ${ post.user.username }</h3>
+    </header>
+    <p>${ post.body }</p>
+    <footer>
+      <div class="comment-list"></div>
+      <a href="#" class="toggle-comments">(<span class="verb">show</span> comments)</a>
+    </footer>
+  </div>`).data('post', post)
+}
+function renderPostList(postList) {
+  $('#app section.active').removeClass('active');
+  const postListElement = $('#post-list');
+  postListElement.empty().addClass('active');
+  postList.forEach(function (post) {
+    postListElement.append( renderPost(post) );
+  });
+}
+function toggleComments(postCardElement) {
+  const footerElement = postCardElement.find('footer');
+  if (footerElement.hasClass('comments-open')) {
+    footerElement.removeClass('comments-open');
+    footerElement.find('.verb').text('show');
+  } else {
+    footerElement.addClass('comments-open');
+    footerElement.find('.verb').text('hide');
+  }
+}
+$('#user-list').on('click', '.user-card .load-posts', function () {
+  const user = $(this).closest('.user-card').data('user');
+  fetchUserPosts(user.id)
+    .then(renderPostList);
+});
+$('#post-list').on('click', '.post-card .toggle-comments', function () {
+  const postCardElement = $(this).closest('.post-card');
+  const post = postCardElement.data('post');
+  const commentListElement = postCardElement.find('.comment-list');
+  setCommentsOnPost(post)
+    .then(function (post) {
+      console.log('building comments for the first time...')
+      commentListElement.empty();
+      post.comments.forEach(function (comment) {
+        commentListElement.prepend($(`
+          <h3>${ comment.body } --- ${ comment.email }</h3>
+        `));
+      });
+      toggleComments(postCardElement);
+    })
+    .catch(function () {
+      console.log('comments previously existed, only toggling...')
+      toggleComments(postCardElement);
+    });
+});
